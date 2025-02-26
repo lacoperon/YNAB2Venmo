e4bf8cceda887297e0b4bdb17ecd9246e27cfbba
lacoperon <elliot.james.williams@gmail.com>
2025-02-25 21:41:22 -0500

    Add .gitignore and implement YNAB token reading functionality

--- ynab_to_venmo.py
+++ ynab_to_venmo.py
@@ -4,5 +4,16 @@ Library to fetch reimbursable transactions from YNAB, and then request payment t
 
 import requests
 
+_TOKEN_FILE = "secrets/ynab_token.txt"
+
+def read_ynab_token() -> None:
+    """
+    Reads the YNAB token from a file.
+    """
+    with open(_TOKEN_FILE, 'r') as file:
+        return file.read().strip()
+    
+
+
 
 def fetch_relevant_transactions(ynab)
